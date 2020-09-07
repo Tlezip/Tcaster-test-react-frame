7230310e913f558fcf0595d919d02c4bdc88ec42
Tlezip <title0088@gmail.com>
2020-09-07 14:11:34 +0700

    initial project, add lodash

---
+++ src/component/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// We're using bulma.io for example styles
+// you can use any css styling as you like
+import './main.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="section">
+        <div className="container">
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
