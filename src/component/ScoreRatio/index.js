9fba5f6566f9b9c98ddf663536360ce8278f14e7
Tlezip <title0088@gmail.com>
2020-09-07 15:45:30 +0700

    add Score Ratio Component

---
+++ src/component/ScoreRatio/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './index.css'
+
+const ScoreRatio = () => (
+  <div className="score-ratio">
+    <div className="score-ratio-icon" />
+    <p className="score-ratio-text">ดูสัดส่วนคะแนน</p>
+  </div>
+)
+
+export default ScoreRatio
